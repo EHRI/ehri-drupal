a0f28db532c26692f8af152dcc0943aa2ef3816f
Mike Bryant <mikesname@gmail.com>
2023-07-04 20:59:03 +0100

    Remove Google Analytics

--- themes/ehricourse/templates/html.tpl.php
+++ themes/ehricourse/templates/html.tpl.php
@@ -40,17 +40,6 @@
     <script src="<?php print $base_path . $path_to_zen; ?>/js/respond.js"></script>
     <![endif]-->
   <?php endif; ?>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-50762933-3', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <?php if ($skip_link_text && $skip_link_anchor): ?>
